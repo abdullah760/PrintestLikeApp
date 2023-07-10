5136741b53df5e314817bdd3ce5c93a0f6207c13
Abdullah760 <abdoassem11@gmail.com>
2023-07-10 12:46:17 -0700

    Initial Commit

---
+++ TheMainProject/TheMainProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMainProjectApp.swift
+//  TheMainProject
+//
+//  Created by Abdullah on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheMainProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
